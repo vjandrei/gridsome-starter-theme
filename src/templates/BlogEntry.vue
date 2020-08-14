d585690821aa5e661d5fc5065a07ba395798596d
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-08-14 13:35:08 +0300

    removed classes

--- src/templates/BlogEntry.vue
+++ src/templates/BlogEntry.vue
@@ -1,40 +1,30 @@
 <template>
   <Layout>
-    <section id="container-centre" class="column centre flex-1">
-      <div class="post-header mb-12 md:mb-20">
-        <h1
-          class="page-title text-3xl md:text-center md:text-5xl lg:text-6xl"
-          v-html="$page.blog.title"
-        ></h1>
-        <div class="text-sm md:text-base text-gray-600 flex justify-center">
-          <p class="author">{{ $page.blog.author.name }}</p>
-          <p class="px-2">—</p>
+    <section>
+      <div>
+        <h1 v-html="$page.blog.title"></h1>
+        <div>
+          <p>{{ $page.blog.author.name }}</p>
           <time :datetime="$page.blog.datetime">{{ $page.blog.humanTime }}</time>
-          <p class="px-2">—</p>
-          <p class="category">
+          <p>
             Posted in
             <g-link :to="$page.blog.category.path">{{ $page.blog.category.title }}</g-link>
           </p>
         </div>
-        <figure class="mt-10 md:mt-20">
+        <figure>
           <g-image :alt="$page.blog.image_caption" :src="$page.blog.image" />
-          <figcaption
-            class="text-center text-sm italic text-gray-600 mt-4"
-          >{{ $page.blog.image_caption }}</figcaption>
+          <figcaption>{{ $page.blog.image_caption }}</figcaption>
         </figure>
       </div>
 
-      <div class="content post md:px-16">
+      <div>
         <p v-html="$page.blog.excerpt"></p>
 
         <div v-html="$page.blog.content"></div>
 
-        <ul class="flex pt-8 border-t border-gray-100">
-          <li class="mr-2" v-for="tag in $page.blog.tags" :key="tag.id">
-            <g-link
-              :to="tag.path"
-              class="inline-block border border-pink-300 px-4 py-2 text-pink-500 text-xs font-semibold rounded hover:text-white hover:bg-pink-500 hover:border-pink-500"
-            >{{ tag.title}}</g-link>
+        <ul>
+          <li v-for="tag in $page.blog.tags" :key="tag.id">
+            <g-link :to="tag.path">{{ tag.title}}</g-link>
           </li>
         </ul>
       </div>
